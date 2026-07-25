e384d3cd0c810ce98aeb6a4966b8662fe4824d56
Dattaraj <dattatraj321@gmail.com>
2026-07-25 19:45:37 +0530

    Time: 1 ms (23.15%) | Memory: 9.1 MB (29.61%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -2,13 +2,28 @@ class Solution {
 public:
     int maxProduct(int n) {
         if(n <= 10) return 0;
+        int fmax = 0;
+        int smax = 0;
         vector<int> store;
         while(n>0){
            store.push_back(n%10);
            n = n/10;
         }
-        sort(store.begin(),store.end());
+        // sort(store.begin(),store.end());
         int size = store.size();
-        return store[size-1]*store[size-2];
+        int fInd = size;
+        for(int i=0;i<size;i++){
+            if(fmax < store[i]){
+                smax = fmax;
+                fmax = store[i];
+                
+            }
+            else if(smax < store[i]){
+                smax = store[i];
+            }
+        }
+        // return store[size-1]*store[size-2];
+        return fmax*smax;
+        
     }
 };
