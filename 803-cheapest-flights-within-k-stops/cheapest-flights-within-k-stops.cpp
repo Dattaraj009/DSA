f363788816bd6a7ab5515f862786741875d36c8e
Dattaraj <dattatraj321@gmail.com>
2026-08-20 13:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (39.04%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n];
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>, greater<tuple<int,int,int>> > pq;
-        for(auto it:flights){
-            adj[it[0]].push_back({it[1],it[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<pair<int, int>> adj[n];
+        for (auto it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        vector<int> dist(n,1e9);
-        dist[src] = 0;
-        
-        pq.push({0,src,0});
-        while(!pq.empty()){
-            auto [steps, node,sum] = pq.top();
+        vector<int> dis(n, 1e9);
+         dis[src] = 0;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<tuple<int, int, int>>>
+            pq;
+        int ans = INT_MAX;
+        pq.push({0, 0, src}); // wt,k,src
+        while (!pq.empty()) {
+            auto [t,wt, node] = pq.top();
             pq.pop();
-            // if(steps > k) continue;
-            for(auto it:adj[node]){
+
+            for (auto it : adj[node]) {
                 int adjnode = it.first;
-                int w = it.second;
-                if(steps <= k && dist[adjnode] > w+sum){
-                    dist[adjnode] = w+sum;
-                    pq.push({steps+1,adjnode,w+sum});
+                int adjwt = it.second;
+                if (t <= k && dis[adjnode] >= adjwt + wt) {
+                    pq.push({ t + 1, adjwt + wt,adjnode});
+                    // vis[adjnode] = 1;
+                    dis[adjnode] = adjwt + wt;
                 }
             }
         }
-        if(dist[dst]==1e9) return -1;
-        return dist[dst];
+
+        if (dis[dst]==1e9)
+            return -1;
+        return dis[dst];
     }
 };
