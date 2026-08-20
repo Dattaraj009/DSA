39aaedd692d21423543522dfd0cf7b32e227acf4
Dattaraj <dattatraj321@gmail.com>
2026-08-20 15:59:14 +0530

    Time: 91 ms (31.74%) | Memory: 47.8 MB (34.08%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -5,29 +5,33 @@ public:
         for(auto it:times){
             adj[it[0]].push_back({it[1],it[2]});
         }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,k});
         vector<int> dis(n+1,1e9);
         dis[0] = 0;
         dis[k] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,k});
         while(!pq.empty()){
-            auto [tw,node] = pq.top();
+            auto [w,node] = pq.top();
             pq.pop();
             for(auto it:adj[node]){
+                int adjw = it.second;
                 int adjnode = it.first;
-                int w = it.second;
-                if(dis[adjnode] > w + tw){
-                    dis[adjnode] = w+tw;
-                    pq.push({w+tw,adjnode});
+                if(dis[adjnode] > adjw+w){
+                    pq.push({adjw+w,adjnode});
+                    dis[adjnode] = adjw+w;
                 }
             }
-            
         }
-        int maxi = INT_MIN;
-        for(int i=1;i<=n;i++){
-            if(dis[i]==1e9) return -1;
-            maxi = max(maxi,dis[i]);
+        int maxi = 0;
+        for(int i=0;i<n+1;i++){
+            if(dis[i]==1e9){
+                return -1;
+            }
+            else{
+                maxi = max(maxi,dis[i]);
+            }
         }
+
         return maxi;
     }
 };
