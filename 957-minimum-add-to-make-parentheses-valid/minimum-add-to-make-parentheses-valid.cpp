b6560013449f2309ff4b3ccef8149886567c7ea8
Dattaraj <dattatraj321@gmail.com>
2026-09-05 18:02:09 +0530

    Time: 3 ms (3.79%) | Memory: 9.5 MB (10.45%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int cnt = 0;
+        int n = s.size();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+           
+            if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                if(!st.empty()) st.pop();
+                else cnt++;
+            }
+        }
+        while(!st.empty()){
+            st.pop();
+            cnt++;
+        }
+        return cnt;
+    }
+};
