0baaf387a11633062084b17739b9b11022136bfb
Dattaraj <dattatraj321@gmail.com>
2026-09-05 14:45:10 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (33.41%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(int l,int r,string s){
+        int n = s.size();
+        while(l<r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++,r--;
+        }
+
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int l=0,r=n-1;
+        while(l<r){
+            if(s[l] != s[r]){
+                return (solve(l+1,r,s) || solve(l,r-1,s));
+            }
+            l++,r--;
+        }
+
+        return true;
+    }
+};
