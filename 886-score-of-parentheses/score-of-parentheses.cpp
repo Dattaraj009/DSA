d7b0981f6dab8639e6dda73861f7149c59476cab
Dattaraj <dattatraj321@gmail.com>
2026-09-12 17:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (67.31%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        int n = s.size();
+        int score = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(score);
+                score = 0;
+            }
+            else{
+                score = st.top() + max(score*2,1);
+                st.pop();
+            }
+        }
+
+        return score;
+    }
+};
